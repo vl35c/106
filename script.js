6d9fad2e967492aacf5c235562f0fbd1fd998ee5
vl35c <124081323+vl35c@users.noreply.github.com>
2023-11-02 23:10:59 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function getTimeUntilDue() {
     } else if (timeUntilDue > 0) {
       e.innerHTML = `${(dueDate - now)} seconds`;
     } else {
-      e.innerHTML = `Due`;
+      e.innerHTML = `${dueDate}`;
     }
   }
   setTimeout("getTimeUntilDue()", 100)
